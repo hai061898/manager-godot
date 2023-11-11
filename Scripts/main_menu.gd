97eb089566e7569d0a11499985da5a1cbb8da5a9
hai061898 <truonghaiauto98@gmail.com>
2023-11-11 18:41:00 +0700

    p2

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,14 +3,36 @@ extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
-@onready var start_level = preload("res://scenes/main_menu.tscn") as PackedScene
+@onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/Option_Button as Button
+@onready var options_menu = $Options_Menu as OptionsMenu
+@onready var margin_container = $MarginContainer as MarginContainer
+
+
+# change start play 
+#@onready var start_level = preload("res://scenes/main_menu.tscn") as PackedScene
 
 func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	exit_button.button_down.connect(on_exit_pressed)
+	handle_connecting_signals()
 
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level)
+#	get_tree().change_scene_to_packed(start_level) 
+	get_tree().quit()	
+
+func on_options_pressed() -> void:
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
 
 func on_exit_pressed() -> void:
 	get_tree().quit()	
+
+func on_exit_options_menu() -> void:
+	margin_container.visible = true
+	options_menu.visible = false
+
+
+func handle_connecting_signals() -> void: 
+	start_button.button_down.connect(on_start_pressed)
+	options_button.button_down.connect(on_options_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
